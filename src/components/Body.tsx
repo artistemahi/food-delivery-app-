1f51a32de63d5547778c8253b6562840715301b0
Mahesh kumar <vickymandal542@gmail.com>
2025-12-10 20:17:44 +0530

    live data fetch

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,16 +1,28 @@
 import reslist from "./../utils/mockdata";
 import RestaurantCard  from "./RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const Body: React.FC = () => {
-  let [RestaurantList, setRestaurantList] = useState(reslist);
+  let [RestaurantList,setRestaurantList] = useState<any[]>([]);
+  // let [RestaurantList,setRestaurantList]= arr;
+  useEffect(()=>{
+    fetchData();
+  },[])
+  const   fetchData = async()=>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.594566&sortBy=RELEVANCE&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    console.log(json);
+    // optional chaining
+    setRestaurantList(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+  }
   return (
     <div className="body">
       <div className="filter">
         <button className="filterbtn"
           onClick={()=>{
-            const filteredList = RestaurantList.filter((res)=>res.info.avgRating > 4.5)
+            const filteredList = RestaurantList.filter((res)=>res.info.avgRating > 4.6
+              )
             setRestaurantList(filteredList);
           }}
         >Top Rated Restaurant</button>
