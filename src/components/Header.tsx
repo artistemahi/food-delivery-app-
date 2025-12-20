dcda3beacfe9b88b6ae248006096ff5b4aedf74a
Mahesh kumar <vickymandal542@gmail.com>
2025-12-20 14:03:16 +0530

    nav Linked

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { logo_Url } from "./../utils/constants";
 import { useState } from "react";
+import { Link } from "react-router";
 const Header: React.FC = () => {
   const [loginBtn, setloginBtn] = useState("Login");
   return (
@@ -8,9 +9,9 @@ const Header: React.FC = () => {
         <img src={logo_Url} alt="company logo" />
       </div>
       <div className="nav">
-        <ul className="item">Home</ul>
-        <ul className="item">About us</ul>
-        <ul className="item">contact us</ul>
+        <ul className="item"><Link to ="/">Home</Link></ul>
+        <ul className="item"><Link to ="/about">About us</Link></ul>
+        <ul className="item"><Link to ="/contact">Contact</Link></ul>
         <ul className="item">cart</ul>
         <button
           className="login"
