6d6228709d40412dde344afe6fe1a13d1fe18cd4
Mahesh kumar <vickymandal542@gmail.com>
2025-12-22 23:45:38 +0530

    menu api fetch

--- src/components/RestaurantMenu.tsx
+++ src/components/RestaurantMenu.tsx
@@ -1,5 +1,18 @@
+import {useEffect,useState} from "react"; 
+import Shimmer from "./Shimmer";
 const RestaurantMenu =()=>{
-    return (
+  const [MenuData, setMenuData] = useState(null);
+    useEffect (()=>{
+    //fetch menu data from api
+    fetchMenu();
+  },[]);
+  const fetchMenu = async()=>{
+    //api call logic
+    const data = await fetch ("https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6426028&lng=77.21921669999999&restaurantId=709678&submitAction=ENTER");
+    const json = await data.json();
+    console.log(json);
+  }
+    return (MenuData===null)? <Shimmer/> : (
         <div className="restaurant-menu">
             <h1>RestaurantName</h1>
             <p>Rating</p>
