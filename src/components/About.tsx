49c15aac7cce8ec26a8db12147feb083ef37aea5
Mahesh kumar <vickymandal542@gmail.com>
2025-12-30 16:01:32 +0530

    classBsComponent

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,6 @@ const about :React.FC = () => {
   return (<>
     <h1>About Us Page</h1>
     <UserClass name = "John Doe" age = "30" location = "New York"/>
-    <User name="deva" age="32" location = "delhi"/>
   </>);
 };
 export default about;
--- src/components/UserClass.tsx
+++ src/components/UserClass.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React from "react";
 
 interface UserProps {
-    name: string;
-    age: string;
-    location: string;
+  name: string;
+  age: string;
+  location: string;
 }
 
-class UserClass extends React.Component<UserProps> {
-    constructor(props: UserProps) {
-        super(props);
-    }
-    render() {
-        return (  
-            <div className="user-card" >
-                <h1>Classed based Component</h1>
-                <h2>Name: {this.props.name}</h2>
-                <h3>Age: {this.props.age}</h3>
-                <h3>Location: India </h3>
-            </div>
-        );
-    }
-};
+interface StateType {
+  count1: number;
+}
+
+class UserClass extends React.Component<UserProps, StateType> {
+  constructor(props: UserProps) {
+    super(props);
+    this.state = {
+      count1: 0,
+    };
+  }
+  render() {
+
+    const { count1 } = this.state;
+    return (
+      <div className="user-card">
+
+        <h1>count : {count1}</h1>
+        <button onClick ={()=>{this.setState(
+            {count1: count1+1}
+        )}} > count increase </button>
+        <h1>Classed based Component</h1>
+        <h2>Name: {this.props.name}</h2>
+        <h3>Age: {this.props.age}</h3>
+        <h3>Location: India </h3>
+      </div>
+    );
+  }
+}
 export default UserClass;
